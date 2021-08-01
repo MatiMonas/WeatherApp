882a87e4304f4a31786f41883cf643f1b957426b
matimonas <matutin97@gmail.com>
2021-08-01 15:16:12 -0300

    adds mediaqueries

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,44 +15,45 @@ export default function Card(props) {
             >
               x
             </button>
+            <div className={style.all}>
+              <Link to={`/ciudad/${props.id}`}>
+                <h1>{props.name}</h1>
+              </Link>
+              <div className={style.status}>
+                <div className={style.mainTemp}>
+                  <h3 className={style.weather}>{props.weather}</h3>
+                  <img
+                    className={style.img}
+                    src={`http://openweathermap.org/img/wn/${props.img}@2x.png`}
+                    alt=""
+                  />
+                  <div className={style.nowTemp}>
+                    <h3>{props.temp}°</h3>
+                    <h6>Feels like {props.feels_like}°</h6>
+                  </div>
+                </div>
 
-            <Link to={`/ciudad/${props.id}`}>
-              <h1>{props.name}</h1>
-            </Link>
-            <div className={style.status}>
-              <div className={style.mainTemp}>
-                <h3 className={style.weather}>{props.weather}</h3>
-                <img
-                  className={style.img}
-                  src={`http://openweathermap.org/img/wn/${props.img}@2x.png`}
-                  alt=""
-                />
-                <div className={style.nowTemp}>
-                  <h3>{props.temp}°</h3>
-                  <h6>Feels like {props.feels_like}°</h6>
-                </div>
-              </div>
-
-              <div className={style.tempContainer}>
-                <div className={style.temps}>
-                  <h6>Min </h6>
-                  <h6>{props.min}°</h6>
-                </div>
-                <div className={style.temps}>
-                  <h6>Max </h6>
-                  <h6>{props.max}°</h6>
-                </div>
-                <div className={style.temps}>
-                  <h6>Humidity </h6>
-                  <h6>{props.humidity}%</h6>
-                </div>
-                <div className={style.temps}>
-                  <h6>Pressure </h6>
-                  <h6>{props.pressure} hPa</h6>
-                </div>
-                <div className={`${style.temps} ${style.wind}`}>
-                  <h6>Wind</h6>
-                  <h6>{props.wind} km/h</h6>
+                <div className={style.tempContainer}>
+                  <div className={style.temps}>
+                    <h6>Min </h6>
+                    <h6>{props.min}°</h6>
+                  </div>
+                  <div className={style.temps}>
+                    <h6>Max </h6>
+                    <h6>{props.max}°</h6>
+                  </div>
+                  <div className={style.temps}>
+                    <h6>Humidity </h6>
+                    <h6>{props.humidity}%</h6>
+                  </div>
+                  <div className={style.temps}>
+                    <h6>Pressure </h6>
+                    <h6>{props.pressure} hPa</h6>
+                  </div>
+                  <div className={`${style.temps} ${style.wind}`}>
+                    <h6>Wind</h6>
+                    <h6>{props.wind} km/h</h6>
+                  </div>
                 </div>
               </div>
             </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../img/logoHenry.png";
+
 import "./styles/Nav.css";
 import { Link } from "react-router-dom";
 import SearchBar from "../components/SearchBar.jsx";
